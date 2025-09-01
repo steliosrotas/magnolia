e5becd22b0c227b0ed29e25f0466f0bedd746b28
Stelios Rotas <steliosrotas@yahoo.com>
2025-09-01 20:30:30 +0300

    added posts model

--- magnoliafarmacy/posts/models.py
+++ magnoliafarmacy/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(height_field=500, width_field=500)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)

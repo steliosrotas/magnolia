dc18b12ab5d9856bc597552dec0775e3e4aa8c15
Stelios Rotas <steliosrotas@yahoo.com>
2025-09-02 13:15:26 +0300

    progress on the post feature

--- magnoliafarmacy/posts/models.py
+++ magnoliafarmacy/posts/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
-    image = models.ImageField(height_field=500, width_field=500)
     body = models.TextField()
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True)
+    banner = models.ImageField(default='fallback.png', blank=True)
 
     def __str__(self):
         return self.title
